11eeaeebf85376209cf9b725222baf7dc447063a
Ritesh Jillewad <130166456+riteshjillewad@users.noreply.github.com>
2026-01-21 22:45:07 +0530

    Update brk_1.c

--- Memory Management/brk_1.c
+++ Memory Management/brk_1.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
 
+int main()
+{
+    void *current_break_point = NULL;
+
+    current_break_point = sbrk(0);
+    printf("Current break point is: %p\n", current_break_point);
+
+    return 0;
+}
