0cff0e5204bac844607f635c60e343d4c7be7ca4
Ritesh Jillewad <130166456+riteshjillewad@users.noreply.github.com>
2026-02-11 17:35:43 +0530

    Add files via upload

--- Assignments/Assignment-8/Question-4/main.c
+++ Assignments/Assignment-8/Question-4/main.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
+#include "helper.h"
 
+int main()
+{
+    int a = 0;
+    int b = 0;
+
+    printf("Enter first number: \n");
+    scanf("%d", &a);
+
+    printf("Enter second number: \n");
+    scanf("%d", &b);
+
+    printf("Addition of %d and %d is: %d\n", a, b, add(a, b));
+    return 0;
+}
